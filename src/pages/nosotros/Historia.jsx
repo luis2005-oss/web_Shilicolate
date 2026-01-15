8100243fce97457429b4ec562db5616e78370ef4
luis2005-oss <solanocortezluisalfredo77@gmail.com>
2026-01-15 11:01:51 -0500

    ADD: se creó el componente Historia y Misión y Visión

---
+++ src/pages/nosotros/Historia.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Historia() {
+  return (
+    <div>
+      <h1>Historia</h1>
+      {/* Tu contenido aquí */}
+    </div>
+  );
+}
+
+export default Historia;
---
+++ src/pages/nosotros/MisionVision.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MisionVision() {
+  return (
+    <div>
+      <h1>Misión y Visión</h1>
+      {/* Tu contenido aquí */}
+    </div>
+  );
+}
+
+export default MisionVision;
