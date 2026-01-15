f7aaa63961dc952bd004ba212f6d078c5b17d151
luis2005-oss <solanocortezluisalfredo77@gmail.com>
2026-01-14 22:00:37 -0500

    ADD: se cambiaron el nombre a algunas carpetas y tbn se agrego el expot default

---
+++ src/pages/Reconocimientos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Reconocimientos() {
+  return (
+    <div>
+      <h1>Reconocimientos</h1>
+      {/* Tu contenido aquí */}
+    </div>
+  );
+}
+
+export default Reconocimientos;
---
+++ src/pages/Trabajadores.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Trabajadores() {
+  return (
+    <div>
+      <h1>Trabajadores</h1>
+      {/* Tu contenido aquí */}
+    </div>
+  );
+}
+
+export default Trabajadores;
