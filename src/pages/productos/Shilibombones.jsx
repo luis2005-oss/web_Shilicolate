3da12cad8aeb240b97f7e0e78d85eb638e10abb1
luis2005-oss <solanocortezluisalfredo77@gmail.com>
2026-01-27 13:10:54 -0500

    ADD: se agregan nuevos sabores y se actualiza la presentación de la información en la página de Shilibombones

--- src/pages/productos/Shilibombones.jsx
+++ src/pages/productos/Shilibombones.jsx
@@ -1,6 +1,42 @@
 import { PiWhatsappLogoBold } from "react-icons/pi";
 import "./Shilibombones.css";
 
+  const lista1 = [
+    {
+      id: 1,
+      titulo: "sabor 1",
+      descripcion: "Elaborado de fruta selecta para lograr un sabor perfecto y armonioso con lo que se puede deleitar, encuentrelo solo aqui."
+    },
+    {
+      id: 2,
+      titulo: "sabor 2",
+      descripcion: "Elaborado de fruta selecta para lograr un sabor perfecto y armonioso con lo que se puede deleitar, encuentrelo solo aqui."
+    },
+    {
+      id: 3,
+      titulo: "sabor 3",
+      descripcion: "Elaborado de fruta selecta para lograr un sabor perfecto y armonioso con lo que se puede deleitar, encuentrelo solo aqui."
+    }
+  ];
+
+  const lista2 = [
+    {
+      id: 1,
+      titulo: "sabor 4",
+      descripcion: "Elaborado de fruta selecta para lograr un sabor perfecto y armonioso con lo que se puede deleitar, encuentrelo solo aqui."
+    },
+    {
+      id: 2,
+      titulo: "sabor 5",
+      descripcion: "Elaborado de fruta selecta para lograr un sabor perfecto y armonioso con lo que se puede deleitar, encuentrelo solo aqui."
+    },
+    {
+      id: 3,
+      titulo: "sabor 6",
+      descripcion: "Elaborado de fruta selecta para lograr un sabor perfecto y armonioso con lo que se puede deleitar, encuentrelo solo aqui."
+    }
+  ];
+
 export default function Shilibombones() {
   return (
     <section className="max-container">
@@ -30,53 +66,36 @@ export default function Shilibombones() {
       <div className="product-description-container">
         <div className="product-description-container-title">
           <article className="product-description-container-title-text">
-            <h4>Sabor 1</h4>
-            <p>Elaborado de fruta selecta para lograr 
-              un sabor perfecto y armonioso con lo 
-              que se puede deleitar, encuentrelo solo aqui.
-            </p>
+            <h4>{lista1[0]?.titulo}</h4>
+            <p>{lista1[0]?.descripcion}</p>
           </article>
           <article className="product-description-container-title-text">
-            <h4>Sabor 1</h4>
-            <p>Elaborado de fruta selecta para lograr 
-              un sabor perfecto y armonioso con lo 
-              que se puede deleitar, encuentrelo solo aqui.
-            </p>
+            <h4>{lista1[1]?.titulo}</h4>
+            <p>{lista1[1]?.descripcion}</p>
           </article>
           <article  className="product-description-container-title-text">
-            <h4>Sabor 1</h4>
-            <p>Elaborado de fruta selecta para lograr 
-              un sabor perfecto y armonioso con lo 
-              que se puede deleitar, encuentrelo solo aqui.
-            </p>
+            <h4>{lista1[2]?.titulo}</h4>
+            <p>{lista1[2]?.descripcion}</p>
           </article>
+
         </div>
           
         <div className="product-description-container-img">
           <img src="/img-prueba.png" alt="producto" />
         </div>
 
-        <div className="product-description-container-title">
-          <article className="product-description-container-title-text">
-            <h4>Sabor 1</h4>
-            <p>Elaborado de fruta selecta para lograr 
-              un sabor perfecto y armonioso con lo 
-              que se puede deleitar, encuentrelo solo aqui.
-            </p>
+        <div className="product-description-container-title2">
+          <article className="product-description-container-title-text2">
+            <h4>{lista2[0]?.titulo}</h4>
+            <p>{lista2[0]?.descripcion}</p>
           </article>
-          <article className="product-description-container-title-text">
-            <h4>Sabor 1</h4>
-            <p>Elaborado de fruta selecta para lograr 
-              un sabor perfecto y armonioso con lo 
-              que se puede deleitar, encuentrelo solo aqui.
-            </p>
+          <article className="product-description-container-title-text2">
+            <h4>{lista2[1]?.titulo}</h4>
+            <p>{lista2[1]?.descripcion}</p>
           </article>
-          <article className="product-description-container-title-text">
-            <h4>Sabor 1</h4>
-            <p>Elaborado de fruta selecta para lograr 
-              un sabor perfecto y armonioso con lo 
-              que se puede deleitar, encuentrelo solo aqui.
-            </p>
+          <article className="product-description-container-title-text2">
+            <h4>{lista2[2]?.titulo}</h4>
+            <p>{lista2[2]?.descripcion}</p>
           </article>
         </div>
       </div>
